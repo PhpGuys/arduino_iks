b9c64b8bfa6eea4988f33e5ddc89edf11cee2000
ksidyagin <64638744+ksidyagin@users.noreply.github.com>
2021-12-11 16:57:32 +0300

    upd

--- hardware/sketch_nov17a/sketch_nov17a.ino
+++ hardware/sketch_nov17a/sketch_nov17a.ino
@@ -1,9 +1,32 @@
+int i = 0;  // переменная для счетчика имитирующего показания датчика
+int led = 13; 
+
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);    // установим скорость обмена данными
+  pinMode(led, OUTPUT);  // и режим работы 13-ого цифрового пина в качестве выхода
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
+  i = i + 1;  // чтобы мы смогли заметить что данные изменились
+  String stringOne = "Info from Arduino ";
+  stringOne += i;  // конкатенация
+  Serial.println(stringOne);  // отправляем строку на порт
 
+  char incomingChar;
+  
+  if (Serial.available() > 0)
+  {
+    // считываем полученное с порта значение в переменную
+    incomingChar = Serial.read();  
+   // в зависимости от значения переменной включаем или выключаем LED
+    switch (incomingChar) 
+    {
+      case '1':
+        digitalWrite(led, HIGH);
+        break;
+      case '0':
+        digitalWrite(led, LOW);
+        break;
+    }
+  }
+  delay(300);
 }
---
+++ software/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SerialPort port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            port.DataReceived += new SerialDataReceivedEventHandler(OnDataReceived);
+            port.Open();
+        }
+
+        private void OnDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            richTextBox1.Invoke(
+                (ThreadStart)delegate ()
+                {
+                    richTextBox1.Text = port.ReadExisting();
+                });
+            Thread.Sleep(1000);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            port.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
