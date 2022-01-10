4b1bb01b8f66c01533d41726623bd59853cadf98
ksidyagin <64638744+ksidyagin@users.noreply.github.com>
2022-01-10 14:58:25 +0300

    upd

---
+++ software/WinFormsApp/Arduino/HardwareMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arduino
+{
+    public class HardwareMessage
+    {
+        public ushort Offset { get; set; }
+        public byte[] Value { get; set; }
+    }
+}
---
+++ software/WinFormsApp/Arduino/PropertyF.cs
@@ -0,0 +1,37 @@
+﻿namespace Arduino
+{
+    public class PropertyF<T> : Property<T> where T : struct, IComparable, IEquatable<T>
+    {
+        TextBox textBox = new TextBox() { Size = new Size(100, 20), Location = new Point(130, 10) };
+        Label label = new Label() { Size = new Size(120, 20), Location = new Point(10, 10), TextAlign = ContentAlignment.MiddleRight };
+
+
+
+        public PropertyF(IHardware hardware, hardParam param) : base( hardware,  param)
+        {
+            textBox.KeyPress += (s, e) =>
+            {
+                if (e.KeyChar == (char)Keys.Return)
+                {
+                    e.Handled = true;
+                    UserSet(textBox.Text.Replace(',', '.'));
+                }
+            };
+            textBox.TextChanged += (s, e) => { focusTimer.Stop(); focusTimer.Start(); };
+
+            textBox.Leave += (s, e) => { focusTimer.Stop(); updateTimer.Start(); UserSet(textBox.Text.Replace(',', '.')); };
+
+            textBox.Enter += (s, e) => { focusTimer.Start(); updateTimer.Stop(); };
+            
+
+
+            label.Text = Name.ToString();
+            _box.Controls.Add(textBox);
+            _box.Controls.Add(label);
+        }
+        public override void Update()
+        {
+            textBox.Text = Value.ToString();
+        }
+    }
+}
